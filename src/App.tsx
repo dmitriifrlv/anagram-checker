04ee109c6387186dacdd0e87406e2c0642d0e56a
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-06-15 17:50:46 -0400

    added anagram checker logic and ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { dbRef } from "./firebase";
 import { get } from "firebase/database";
 
+const wordNormalizer = (word: string) =>
+  word.toLowerCase().split("").sort().join("");
+
+const anagramChecker = (element: string, text: string) => {
+  return wordNormalizer(element) === wordNormalizer(text);
+};
+
 function App() {
+  const [text, setText] = useState("");
+  const [data, setData] = useState<string[] | null>(null);
+  const [numOfAnagrams, setNumOfAnagrams] = useState(0);
+
   get(dbRef)
     .then((snapshot) => {
       if (snapshot.exists()) {
-        console.log(snapshot.val());
+        if (!data) {
+          setData(snapshot.val().test);
+        }
       } else {
         console.log("No data available");
       }
@@ -13,7 +27,42 @@ function App() {
     .catch((error) => {
       console.error(error);
     });
-  return <div className="App"></div>;
+
+  useEffect(() => {
+    if (text !== "" && data) {
+      let num = 0;
+      data.forEach((element) => {
+        if (anagramChecker(element, text)) {
+          num += 1;
+          return num;
+        }
+      });
+      setNumOfAnagrams(num);
+    }
+  }, [text, data]);
+
+  return (
+    <div className="App">
+      <div>
+        <input
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          type="text"
+        />
+        <p>
+          {numOfAnagrams} anagram{numOfAnagrams === 1 ? "" : "s"} found
+        </p>
+      </div>
+      {data && (
+        <div>
+          <p>Data:</p>
+          <code>
+            {JSON.stringify(data.filter((element) => Boolean(element)))}
+          </code>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
